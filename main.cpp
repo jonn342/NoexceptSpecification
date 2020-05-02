51adbf4a7d0f71a93ea76350bde633db8347a1b9
jonn342 <vladislavkurashev1995@yandex.ru>
2020-05-02 22:39:23 +0300

    noexcept declval

---
+++ main.cpp
@@ -0,0 +1,22 @@
+﻿#include <utility>
+#include <iostream>
+
+/*
+*  Здесь мы имеем функцию foo() со спецификацией noexcept,
+*  проверяющей, не будут ли бросать исключение сложение двух объектов типа Т,
+*  копирование объектов типа Т и присваивание объектов типа Т
+*
+*/
+
+template <class T>
+void do_math() noexcept(noexcept (std::declval<T>() + std::declval<T>()) && std::is_nothrow_copy_constructible<T>::value && std::is_nothrow_assignable<T&, T>::value)
+{
+	//что-то делаем
+}
+
+
+int main() {
+	bool a = noexcept(do_math<int>());  //true
+	bool b = noexcept(do_math<std::string>());  //false
+	return 0;
+}
